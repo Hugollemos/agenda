808ba1b39b2a4e562bd386629de869ee34dc4867
hugo lemos <hugolemosdasilva.hu@gmail.com>
2021-10-26 08:41:39 -0300

    criando as rotas

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 // Carregando módulos
   const express = require('express')
   const handlebars = require('express-handlebars')
-  const bodyParser = require("body-parser")
   const app = express()
   const admin = require("./routes/admin")
   //const mongoose = require("mongoose")
 //Configurações
   //body Parser
-    app.use(bodyParser.urlencoded({extended: true}))
-    app.use(bodyParser.json())
+  app.use(express.json());
+  app.use(express.urlencoded({extended:true}))
   //Handlebars
     app.use(bodyParser.urlencoded({extended:true}))
     app.use(bodyParser.json())
   // Mongoose
 // Rotas
-  app.use("/admin", admin)
+  app.get('/', (req, res) => {
+    res.send('Rota principal')
+  })
+
+  app.get('/posts', (req, res) => {
+    res.send("lista Posts")
+  })
+
+  app.use('/admin', admin)
 // Outros
 const PORT = 8081
 app.listen(PORT,() => {
