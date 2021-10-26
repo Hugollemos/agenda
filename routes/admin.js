f34459e8020f86c4cb7487b84ae7fedea1042d2e
hugo lemos <hugolemosdasilva.hu@gmail.com>
2021-10-26 06:55:30 -0300

    Formulário de categorias

--- routes/admin.js
+++ routes/admin.js
@@ -10,7 +10,15 @@ router.get('/posts', (req, res) => {
 })
 
 router.get("/categorias", (req, res) => {
-  res.send("Página de categorias")
+  res.render("admin/categorias")
+})
+
+router.get('/categorias/add', (req, res) => {
+  res.render("admin/addcategorias")
+})
+
+router.get("/teste", (req, res) => {
+  res.send("Isso é um teste")
 })
 
 module.exports = router
