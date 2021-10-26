79c3a87f5eaa8b67422d6fffc5d55d93ba7c66b0
hugo lemos <hugolemosdasilva.hu@gmail.com>
2021-10-26 09:14:53 -0300

    adicionando arquivos estáticos e modf a main

--- app.js
+++ app.js
@@ -3,15 +3,19 @@
   const handlebars = require('express-handlebars')
   const app = express()
   const admin = require("./routes/admin")
+  const path = require("path")
   //const mongoose = require("mongoose")
 //Configurações
   //body Parser
-  app.use(express.json());
   app.use(express.urlencoded({extended:true}))
+  app.use(express.json());
   //Handlebars
-    app.use(bodyParser.urlencoded({extended:true}))
-    app.use(bodyParser.json())
+    app.engine('handlebars', handlebars({defaultLayout: 'main'}))
+    app.set('view engine', 'handlebars');
   // Mongoose
+    // em breve
+  // Public
+    app.use(express.static(path.join(__dirname, 'public')))
 // Rotas
   app.get('/', (req, res) => {
     res.send('Rota principal')
